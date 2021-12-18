0896c34588cfc476a5ec35db7bfece9cbd22536a
Ignasius Yuda Adhitia <ignasius.yuda.adhitia@gmail.com>
2021-12-18 10:43:48 +0700

    Pengumpulan Tugas 2 JS - NIOMIC

---
+++ tugas2.js
@@ -0,0 +1,51 @@
+var first = 6;
+const second = ["number", "array", "string", "boolean", "null", "undefined"];
+let third = "NIOMIC";
+var fourth = true;
+const fifth = null;
+let sixth;
+
+console.log("I have", first, "variables, with various values:");
+console.log(
+  "The first variable value is",
+  first + ".",
+  "It is a",
+  second[0],
+  "data."
+);
+console.log(
+  "The second variable value is",
+  second,
+  ".",
+  "It is an",
+  second[1],
+  "data."
+);
+console.log(
+  "The third variable value is",
+  third + ".",
+  "It is a",
+  second[2],
+  "data."
+);
+console.log(
+  "The fourth variable value is",
+  fourth + ".",
+  "It is a",
+  second[3],
+  "data."
+);
+console.log(
+  "The fifth variable value is",
+  fifth + ".",
+  "It is a",
+  second[4],
+  "data."
+);
+console.log(
+  "The sixth variable value is",
+  sixth + ".",
+  "It is an",
+  second[5],
+  "data."
+);
